1092f1d4f732161349a88bdbc74b5f7c326c8e32
RenYifei98 <2549884312@qq.com>
2019-03-11 11:39:32 +0800

    springboot-experiment-01 debug test

--- src/test/java/com/expreiment/springbootexperiment01/SpringbootExperiment01ApplicationTests.java
+++ src/test/java/com/expreiment/springbootexperiment01/SpringbootExperiment01ApplicationTests.java
@@ -1,6 +1,8 @@
 package com.expreiment.springbootexperiment01;
 
 import com.expreiment.springbootexperiment01.repository.UserRepository;
+import com.expreiment.springbootexperiment01.entity.User;
+import com.expreiment.springbootexperiment01.entity.Address;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
